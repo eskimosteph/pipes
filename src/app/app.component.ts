dd3b0c9719c9b07e1787f0ecca6e40f54cf6a3e1
OsvaldoGD <reynoso.renteria.osvaldo@gmail.com>
2019-10-07 12:12:01 -0700

    Pipes: Decimal, Percent, Currency, Json, Async, Date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,24 @@ export class AppComponent {
   arreglo = [ 1,2,3,4,5,6,7,8,9 ];
 
   PI = Math.PI;
+
+  a = 0.234;
+
+  salario = 1234.5;
+
+  heroe = {
+    nombre: "Peter",
+    clave: "Spiderman",
+    edad: 24,
+    dirrecion: {
+      ciudad: "Manhattan",
+      casa: 2
+    }
+  }
+
+  valorDePromesa = new Promise( (resolve, reject) => {
+    setTimeout( () => resolve('Llego la data!'), 3500 )
+  });
+
+  fecha = new Date();
 }
